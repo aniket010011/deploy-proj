940a4e563c2d30a74d27a42235c6f29e4f660dfd
aniket010011 <149388350+aniket010011@users.noreply.github.com>
2025-11-12 19:27:24 +0530

    Update app title and description

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('📊 Financial Assessment and EMI Prediction Project')
 
-st.write('Hello world!')
+st.info('This app conducts exploratory data analysis and predicts EMI amount and eligibility')
