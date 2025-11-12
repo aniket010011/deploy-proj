08beb77b89ccfc72204f47684b8d1629a7d16a46
aniket010011 <149388350+aniket010011@users.noreply.github.com>
2025-11-12 21:26:42 +0530

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title('📊 Financial Assessment and EMI Prediction Project')
-
-st.info('This app conducts exploratory data analysis and predicts EMI amount and eligibility')
